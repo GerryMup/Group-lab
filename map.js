8a54ed935e40d0b43bbd87f9c17b1352367bdc23
Keanu Naidoo <1422973@students.wits.ac.za>
2019-04-24 13:51:18 +0200

    User Story Itenary

--- map.js
+++ map.js
@@ -8,3 +8,46 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   id: 'mapbox.streets'
 }).addTo(mymap)
+
+let itenary = [
+  {
+    name: 'Russia',
+    lattitude: 61.5240,
+    longnitude: 105.3188
+
+  },
+
+  {
+    name: 'UK',
+    lattitude: 51.5074,
+    longnitude: 0.1278
+  },
+
+  {
+    name: 'Egypt',
+    lattitude: 26.8206,
+    longnitude: 30.8025
+
+  },
+
+  {
+    name: 'France',
+    lattitude: 48.8566,
+    longnitude: 2.3522
+  }
+]
+
+// let marker = L.marker([51.5, -0.09]).addTo(mymap)
+
+let markers = []
+
+for (index in itenary) {
+  let ul = document.getElementById('dynamic-list')
+  let li = document.createElement('li')
+  li.className = 'list-group-item'
+  li.appendChild(document.createTextNode(itenary[index].name))
+  ul.appendChild(li)
+}
+
+// let marker = L.marker([55, 22]).addTo(mymap)
+// markers.push(marker)
